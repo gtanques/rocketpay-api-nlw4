d576900be81fca1f43449aeaebf5c5cc6af9ed0e
gtanques <saintcalm1@gmail.com>
2021-04-28 19:17:15 -0300

    Adicionado deposito de conta, alteraçoes no esquema de conta

--- lib/rocketpay.ex
+++ lib/rocketpay.ex
@@ -1,5 +1,8 @@
 defmodule Rocketpay do
+
+  alias Rocketpay.Account.Deposit, as: AccountDeposit
   alias Rocketpay.Users.Create, as: UserCreate
 
   defdelegate create_user(params), to: UserCreate, as: :call
+  defdelegate deposit(params), to: AccountDeposit, as: :call
 end
