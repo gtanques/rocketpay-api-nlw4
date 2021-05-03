f90ce10ad26a66ce925d99d337d1eb4949fe5a6f
gtanques <saintcalm1@gmail.com>
2021-05-03 18:46:10 -0300

    refatoraçao saque e deposito

--- lib/rocketpay.ex
+++ lib/rocketpay.ex
@@ -1,5 +1,4 @@
 defmodule Rocketpay do
-
   alias Rocketpay.Accounts.Deposit, as: AccountDeposit
   alias Rocketpay.Accounts.Withdraw, as: AccountWithdraw
   alias Rocketpay.Users.Create, as: UserCreate
--- lib/rocketpay_web/views/account_view.ex
+++ lib/rocketpay_web/views/account_view.ex
@@ -2,12 +2,12 @@ defmodule RocketpayWeb.AccountView do
   alias Rocketpay.Account
 
   def render("update.json", %{account: %Account{id: account_id, balance: balance}}) do
-      %{
-        message: "Ballance changed successfully!",
-          account: %{
-            id: account_id,
-            balance: balance
-        }
+    %{
+      message: "Ballance changed successfully!",
+      account: %{
+        id: account_id,
+        balance: balance
       }
-    end
+    }
+  end
 end
